c8fd28b6ac82216a000891d58861ea6811363617
Evgeniy Kirov <me@xtr.pp.ru>
2011-03-06 22:26:58 +0300

    Попробуем починить флэшплеер...

--- tvoeradio/radio/static/radio/js/main.js
+++ tvoeradio/radio/static/radio/js/main.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
     // Поднимаем плеер
 
     $("#mp3player").jPlayer({
-        'swfPath': config.jplayer_swfpath.replace(/\\/g, '/').replace(/\/[^\/]*\/?$/, ''),
+        'swfPath': config.jplayer_swfpath,
         'play': function(e) {
             network.lastfm.api(
                 'track.updateNowPlaying',
