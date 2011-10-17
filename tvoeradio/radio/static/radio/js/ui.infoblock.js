e9e1d15288b687719903fbb3744a777ce6555765
Evgeniy Kirov <evg.kirov@gmail.com>
2011-10-17 07:36:04 +0300

    Новая заменялка ссылок.

--- tvoeradio/radio/static/radio/js/ui.infoblock.js
+++ tvoeradio/radio/static/radio/js/ui.infoblock.js
@@ -16,6 +16,18 @@ ui.infoblock.show_artist = function(elem, name) {
                 'image': data.artist.image[data.artist.image.length-1]["#text"]
             };
             elem.html(ich.tpl_infoblock_artist(context));
+            // TODO template
+            $('.infoblock__text a').each(function(){
+                var html = '<span>' + $(this).text() + '</span>';
+                if ($(this).hasClass('bbcode_artist')) {
+                    var artist = $(this).attr('href');
+                    artist = artist.replace('http://www.last.fm/music/', '');
+                    artist = util.string.urldecode(artist);
+                    html = '<span class="pseudolink nav-infoblock" data-type="artist" data-name="' + artist + '">' + $(this).text() + '</span>'
+                }
+                $(html).insertAfter($(this));
+                $(this).remove();
+            });
             //var id = 'infoblock_artist' + util.random.randint(0, 100500);
             //elem.find('.infoblock__comments').attr('id', id + '__comments');
             //network.vkontakte.Widgets.Comments(id + '__comments', {autoPublish: 0, limit: 5}, util.string.md5('artist ' + data.artist.name));
@@ -42,11 +54,4 @@ $('.nav-infoblock').live('click', function(e) {
 });
 
 
-$('a.bbcode_artist').live('click', function(e) {
-    e.preventDefault();
-    var artist = $(this).attr('href');
-    artist = artist.replace('http://www.last.fm/music/', '');
-    artist = util.string.urldecode(artist);
-    ui.infoblock.show_popup('artist', artist)
-});
 
