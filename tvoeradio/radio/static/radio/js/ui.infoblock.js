4a197e9e75fc048dd18c8f8169bda5c0ccd938c3
Evgeniy Kirov <evg.kirov@gmail.com>
2011-11-13 16:55:57 +0300

    Фикс для комментариев.

--- tvoeradio/radio/static/radio/js/ui.infoblock.js
+++ tvoeradio/radio/static/radio/js/ui.infoblock.js
@@ -26,7 +26,7 @@ ui.infoblock.add_comments = function(elem, type, name) {
     var hash = util.string.md5(type + ' ' + name)
     var id = 'infoblock_artist' + util.random.randint(0, 100500);
     elem.find('.infoblock__comments').attr('id', id + '__comments');
-    network.vkontakte.Widgets.Comments(id + '__comments', {autoPublish: 0, limit: 5});
+    network.vkontakte.Widgets.Comments(id + '__comments', {autoPublish: 0, limit: 5}, hash);
 }
 
 
