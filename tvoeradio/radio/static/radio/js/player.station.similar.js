e76b044f0e060ade57bf21d9173bca2ad9b5d3c3
Evgeniy Kirov <evg.kirov@gmail.com>
2012-01-04 19:23:27 +0400

    Убрано ненужное обращение к ластфму в similar.js

--- tvoeradio/radio/static/radio/js/player.station.similar.js
+++ tvoeradio/radio/static/radio/js/player.station.similar.js
@@ -4,9 +4,7 @@ register_namespace('player.station.similar');
 player.station.similar.add_to_playlist = function(callback) {
     network.lastfm.api('artist.getSimilar', {'artist': player.station.name}, function(data) {
         var sim_artist = util.random.choice_similar(network.lastfm.arrayize(data.similarartists.artist));
-        network.lastfm.api('artist.getTopTracks', {'artist': sim_artist.name, 'limit': 50}, function(data) {
-            player.station.common.play_track_by_artist(sim_artist.name, callback);
-        });
+        player.station.common.play_track_by_artist(sim_artist.name, callback);
     });
 };
 
