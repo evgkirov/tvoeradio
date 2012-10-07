2a7ed93d681042755010becb3416521ba80aaaca
Evgeniy Kirov <evg.kirov@gmail.com>
2012-10-08 01:43:49 +0400

    Не следует экранировать обратный слэш.

--- tvoeradio/static/js/3rdparty/ICanHaz.js
+++ tvoeradio/static/js/3rdparty/ICanHaz.js
@@ -73,7 +73,7 @@ var Mustache = function() {
             this.ctag);
       return template.replace(regex, function(match, pragma, options) {
         if(!that.pragmas_implemented[pragma]) {
-          throw({message: 
+          throw({message:
             "This implementation of mustache doesn't understand the '" +
             pragma + "' pragma"});
         }
@@ -254,7 +254,7 @@ var Mustache = function() {
       return s.replace(/&(?!\w+;)|["<>\\]/g, function(s) {
         switch(s) {
         case "&": return "&amp;";
-        case "\\": return "\\\\";
+        // case "\\": return "\\\\";
         case '"': return '\"';
         case "<": return "&lt;";
         case ">": return "&gt;";
@@ -337,22 +337,22 @@ function ICanHaz() {
     self.VERSION = "0.9";
     self.templates = {};
     self.partials = {};
-    
+
     // public function for adding templates
     // We're enforcing uniqueness to avoid accidental template overwrites.
     // If you want a different template, it should have a different name.
     self.addTemplate = function (name, templateString) {
         if (self[name]) throw "Invalid name: " + name + ".";
         if (self.templates[name]) throw "Template \" + name + \" exists";
-        
+
         self.templates[name] = templateString;
         self[name] = function (data, raw) {
             data = data || {};
             var result = Mustache.to_html(self.templates[name], data, self.partials);
             return raw ? result : $(result);
-        };       
+        };
     };
-    
+
     // public function for adding partials
     self.addPartial = function (name, templateString) {
         if (self.partials[name]) {
@@ -361,22 +361,22 @@ function ICanHaz() {
             self.partials[name] = templateString;
         }
     };
-    
+
     // grabs templates from the DOM and caches them.
     // Loop through and add templates.
-    // Whitespace at beginning and end of all templates inside <script> tags will 
-    // be trimmed. If you want whitespace around a partial, add it in the parent, 
+    // Whitespace at beginning and end of all templates inside <script> tags will
+    // be trimmed. If you want whitespace around a partial, add it in the parent,
     // not the partial. Or do it explicitly using <br/> or &nbsp;
-    self.grabTemplates = function () {        
+    self.grabTemplates = function () {
         $('script[type="text/html"]').each(function (a, b) {
             var script = $((typeof a === 'number') ? b : a), // Zepto doesn't bind this
                 text = (''.trim) ? script.html().trim() : $.trim(script.html());
-            
+
             self[script.hasClass('partial') ? 'addPartial' : 'addTemplate'](script.attr('id'), text);
             script.remove();
         });
     };
-    
+
     // clears all retrieval functions and empties caches
     self.clearAll = function () {
         for (var key in self.templates) {
@@ -385,7 +385,7 @@ function ICanHaz() {
         self.templates = {};
         self.partials = {};
     };
-    
+
     self.refresh = function () {
         self.clearAll();
         self.grabTemplates();
