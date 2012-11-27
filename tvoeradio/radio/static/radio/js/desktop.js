b4f791c01d5af3bbae72282631de1996b6488a6a
Evgeniy Kirov <evg.kirov@gmail.com>
2012-11-27 18:35:31 +0400

    Отключение (временное) wall.post в Десктопе.

--- tvoeradio/radio/static/radio/js/desktop.js
+++ tvoeradio/radio/static/radio/js/desktop.js
@@ -4,4 +4,12 @@ if (window['bridge']) {
     bridge.pause.connect(player.control.pause);
     bridge.next.connect(player.control.next);
 
+    // TODO fix this
+    $(document).ready(function(){
+        $('#menu_track__poststatus').remove();
+        $('#menu_track__postwall').remove();
+        $('#menu_station__poststatus').remove();
+        $('#menu_station__postwall').remove();
+    });
+
 }
